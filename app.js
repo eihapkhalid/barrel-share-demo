b5c8c1a6ad4fef4cc0b60f8733064c29801ccf05
eihap khalid <82775669+eihapkhalid@users.noreply.github.com>
2026-08-15 15:10:28 +0300

    Load main app and Excel import addon

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 (() => {
-  const script = document.createElement('script');
-  script.src = 'main.js';
-  script.defer = true;
-  document.head.appendChild(script);
+  const main = document.createElement('script');
+  main.src = 'main.js';
+  main.onload = () => {
+    const excelImport = document.createElement('script');
+    excelImport.src = 'excel-import.js';
+    document.head.appendChild(excelImport);
+  };
+  document.head.appendChild(main);
 })();
